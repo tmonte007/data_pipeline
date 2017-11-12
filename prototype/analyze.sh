cb353636419f6c127411b9c03ca55dd00521c8c4
Tom Monte <tmonte@gmail.com>
2017-11-11 21:50:48 -0500

    Update analyze.sh

--- prototype/analyze.sh
+++ prototype/analyze.sh
@@ -5,7 +5,7 @@
 cd /opt/spark/
 sudo /opt/spark/bin/spark-submit \
    --class "com.databricks.apps.logs.LogAnalyzerAppMain" \
-   --master spark://192.168.1.25:4040 \
+   --master spark://192.168.1.25:7077 \
    /opt/log_analyzer/reference-apps/logs_analyzer/app/java8/target/uber-log-analyzer-2.0.jar \
    --logs-directory /opt/access_log/ \
    --output-html-file /tmp/log_stats.html \
