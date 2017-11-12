18ce484511cae15f730a3966b42b31fa131ca858
Tom Monte <tmonte@gmail.com>
2017-11-11 20:20:19 -0500

    Update common_install.sh

--- prototype/common_install.sh
+++ prototype/common_install.sh
@@ -4,10 +4,11 @@
 sudo apt-add-repository ppa:webupd8team/java
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install oracle-java8-installer
-sudo apt-get install lynx
+sudo apt-get install -y oracle-java8-installer
+sudo apt-get install lynx -y
 
 # Install Apache Spark
 cd /opt/
