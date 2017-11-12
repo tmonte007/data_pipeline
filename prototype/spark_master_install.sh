89d21b4e8965e116f555d609bc702a3b5e39cec1
Tom Monte <tmonte@gmail.com>
2017-11-11 20:48:27 -0500

    Update spark_master_install.sh

--- prototype/spark_master_install.sh
+++ prototype/spark_master_install.sh
@@ -25,15 +25,3 @@ sudo /opt/maven/bin/mvn package
 
 # Reboot
 sudo reboot
-
-# Use the log analyzer
-cd /opt/spark/
-sudo /opt/spark/bin/spark-submit \
-   --class "com.databricks.apps.logs.LogAnalyzerAppMain" \
-   --master spark://192.168.1.25:4040 \
-   /opt/log_analyzer/reference-apps/logs_analyzer/app/java8/target/uber-log-analyzer-2.0.jar \
-   --logs-directory /opt/access_log/ \
-   --output-html-file /tmp/log_stats.html \
-   --window-length 30 \
-   --slide-interval 5 \
-   --checkpoint-directory /tmp
