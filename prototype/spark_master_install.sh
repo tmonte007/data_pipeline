b4993d15736324d76f5812afe95ff503eeffc32a
Tom Monte <tmonte@gmail.com>
2017-11-11 20:37:57 -0500

    Update spark_master_install.sh

--- prototype/spark_master_install.sh
+++ prototype/spark_master_install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
+# Name the host
 sudo hostnamectl set-hostname sparkmaster
-
-# start apache spark
-sudo /opt/spark/sbin/start-master.sh &
 
 # maven install
 cd /opt/
-wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+sudo wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 sudo tar -xvzf apache-maven-3.3.9-bin.tar.gz
 sudo mv apache-maven-3.3.9 maven 
 sudo rm apache-maven-3.3.9-bin.tar.gz
@@ -24,14 +22,23 @@ sudo git clone https://github.com/databricks/reference-apps.git
 cd /opt/log_analyzer/reference-apps/logs_analyzer/app/java8
 sudo /opt/maven/bin/mvn package
 
+# start apache spark
+sudo tee -a /etc/hosts <<<'192.168.1.25  sparkmaster'
+sudo /opt/spark/sbin/start-master.sh &
+
 # Use the log analyzer
 cd /opt/spark/
 sudo /opt/spark/bin/spark-submit \
    --class "com.databricks.apps.logs.LogAnalyzerAppMain" \
    --master spark://192.168.1.25:4040 \
-   target/uber-log-analyzer-2.0.jar \
+   /opt/log_analyzer/reference-apps/logs_analyzer/app/java8/target/uber-log-analyzer-2.0.jar \
    --logs-directory /opt/access_log/ \
    --output-html-file /tmp/log_stats.html \
    --window-length 30 \
    --slide-interval 5 \
    --checkpoint-directory /tmp
+   
+      /opt/log_analyzer/reference-apps/logs_analyzer/app/java8/target/log-analyzer-2.0-shaded.jar
+      
+      Cannot assign requested address: Service 'sparkDriver' failed after 16 retries (on a random free port)! Consider explicitly setting the appropriate binding address for the service 'sparkDriver' (for example spark.driver.bindAddress for SparkDriver) to the correct binding address.
+
